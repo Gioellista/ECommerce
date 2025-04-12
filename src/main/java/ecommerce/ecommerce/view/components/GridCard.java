7376e1fa0ab6a9361ae2cb52e86ec6ab497ef0f0
Gioele Muscolino <gioele.muscolino@gmail.com>
2025-04-12 09:38:48 +0200

    Commit 006 - Database works

--- src/main/java/ecommerce/ecommerce/view/components/GridCard.java
+++ src/main/java/ecommerce/ecommerce/view/components/GridCard.java
@@ -4,10 +4,11 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import ecommerce.ecommerce.data.Product;
 
 @CssImport("./styles/dynamicGridStyle.css")
-public class GridCard extends Div {
+public class GridCard extends VerticalLayout {
 
     Product product;
 
@@ -15,6 +16,10 @@ public class GridCard extends Div {
         this.product = product;
         setClassName("grid-card");
 
-        add(new Text("Card"));
+        add(
+                new H3(product.getTitle()),
+                new Div(product.getDescription()),
+                new Div(String.valueOf(product.getPrice()))
+        );
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (title, description, price) VALUES
+('Laptop', 'Description of Laptop', 899.99),
+('Smartphone', 'Description of Smartphone', 359.99);
